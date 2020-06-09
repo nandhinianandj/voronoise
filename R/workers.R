b3b2086a6f39df5fc3cb2f06cdf1d55ffe64bb2a
Nandhini Anand <cattykatrina4@gmail.com>
2020-06-10 01:55:25 +0530

    Auto committing

---
+++ R/workers.R
@@ -0,0 +1,110 @@
+# the three user-facing functions of the package
+
+# functions that do the bulk of the work
+
+
+# the very first shoot is the "sapling"
+#' @export
+grow_sapling <- function(startx=0, starty=0) {
+
+  sapling <- tibble::tibble(
+    x_0 = startx, y_0 = starty,  # first shoot starts at origin
+    x_1 = startx, y_1 = .5 * starty, # first shoot guide is its midpoint
+    x_2 = startx, y_2 = 1 * starty,  # first shoot grow to y = 1
+    seg_deg = 0,      # segment orientation is vertical
+    seg_len = 1,       # segment length is 1
+    id_time = 1L       # the acorn grows at "time 1"
+  )
+  return(sapling)
+}
+
+
+# for each existing shoot on the tree, grow an additional shoot that
+# extends it; then prune some of them away
+grow_shoots <- function(time, shoots, param) {
+
+  n_shoots <- nrow(shoots)
+  n_pruned <- stats::rbinom(n = 1, size = n_shoots - 1, prob = param$prune)
+
+  ch_seg_len <- dqsample::sample(x = param$scale, size = n_shoots, replace = TRUE)
+  ch_seg_deg <- dqsample::sample(x = param$angle, size = n_shoots, replace = TRUE)
+
+  shoots <- shoots %>%
+    dplyr::mutate(
+      x_0 = x_2,
+      y_0 = y_2,
+      seg_len = seg_len * ch_seg_len,
+      x_1 = x_0 + extend_x(seg_len/2, seg_deg),
+      y_1 = y_0 + extend_y(seg_len/2, seg_deg),
+      seg_deg = seg_deg + ch_seg_deg,
+      id_time = id_time + 1L,
+      x_2 = x_0 + extend_x(seg_len, seg_deg) ,
+      y_2 = y_0 + extend_y(seg_len, seg_deg),
+    ) %>%
+    dplyr::sample_n(size = n_shoots - n_pruned)
+
+  return(shoots)
+}
+
+
+# to grow a "layer" of the shrub, we extend (and possibly prune) each
+# existing shoot multiple times
+grow_layer <- function(shoots, time, param) {
+
+  new_shoots <- purrr::map_dfr(
+    .x = 1:param$split,
+    .f = grow_shoots,
+    shoots = shoots,
+    param = param
+  )
+  return(new_shoots)
+}
+
+
+# to grow the whole tree we need to "accumulate" the growth: starting with
+# the sapling (a single shoot) we grow the second layer; the set of shoots
+# that make the second layer are then used to grow the third later; and so on
+grow_tree <- function(sapling, param) {
+
+  tree <- purrr::accumulate(
+    .x = 1:param$time,
+    .f = grow_layer,
+    .init = sapling,
+    param = param
+  )
+  return(tree)
+}
+
+
+# the data structure that we used to grow the tree is designed to allow
+# efficient computation, but is not optimal for ggplot3 so it needs to
+# be reshaped into a convenient form
+shape_tree <- function(tree) {
+
+  tree <- tree %>%
+    dplyr::bind_rows() %>%
+    dplyr::mutate(id_path = as.integer(1:dplyr::n())) %>%
+    tidyr::pivot_longer(
+      cols = x_0:y_2,
+      names_to = "id_step",
+      values_to = "coord"
+    ) %>%
+    tidyr::separate(col = id_step, into = c("axis", "id_step")) %>%
+    tidyr::pivot_wider(names_from = axis, values_from = coord) %>%
+    dplyr::mutate(
+      id_step = as.integer(id_step),
+      seg_col = sqrt(x ^ 2 + y ^ 2) + (seg_deg - 90) / 10,
+      seg_wid = exp(-id_time^2 / 10)
+    ) %>%
+    dplyr::rename(coord_x = x, coord_y = y) %>%
+    dplyr::select(
+      coord_x, coord_y, seg_deg, seg_len, seg_col, seg_wid,
+      id_time, id_path, id_step
+    )
+
+  return(tree)
+}
+
+
+
+
